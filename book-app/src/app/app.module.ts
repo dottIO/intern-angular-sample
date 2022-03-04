0bb8ad229fe41e62540b6e1234a1e2614c5de594
kusano shunsuke <kusano@thedott.io>
2022-03-04 19:48:19 +0900

    #7 delete extra code and update css class name

--- book-app/src/app/app.module.ts
+++ book-app/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +16,6 @@ import { ListComponent } from './pages/list/list.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule
